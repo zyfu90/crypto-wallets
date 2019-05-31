e4a4773f13a146e52b8b0165c7d74fde32978ed9
Eric Fu <eric.fu@sdce.com.au>
2019-05-31 17:16:17 +1000

    add bitcoin-abc

--- bitcoin-abc/entrypoint.sh
+++ bitcoin-abc/entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-sed -i "s|rpcport=.*|rpcport=${RPC_PORT}|" ./*.conf
-sed -i "s|rpcuser=.*|rpcuser=${RPC_USER}|" ./*.conf
-sed -i "s|rpcpassword=.*|rpcpassword=${RPC_PASSWORD}|" ./*.conf
-sed -i "s|rpcallowip=.*|rpcallowip=${RPC_ALLOW_IP}|" ./*.conf
+sed -i "s|rpcport=.*|rpcport=${RPC_PORT}|g" ./template.conf
+sed -i "s|rpcuser=.*|rpcuser=${RPC_USER}|g" ./template.conf
+sed -i "s|rpcpassword=.*|rpcpassword=${RPC_PASSWORD}|g" ./template.conf
+sed -i "s|rpcallowip=.*|rpcallowip=${RPC_ALLOW_IP}|g" ./template.conf
+sed -i "s|SYMBOL|${SYMBOL}|g" ./template.conf
 
 
 if [ "${ENV}" == "mainnet" ];
 then
-    /root/bitcoin-abc/bin/bitcoind -rescan -datadir=/root/mainnet_data -conf=/root/mainnet.conf
+    /root/bitcoin-abc/bin/bitcoind -rescan -datadir=/root/mainnet_data -conf=/root/template.conf
 else
-    /root/bitcoin-abc/bin/bitcoind -rescan -regtest -datadir=/root/testnet_data -conf=/root/testnet.conf
+    /root/bitcoin-abc/bin/bitcoind -rescan -regtest -datadir=/root/testnet_data -conf=/root/template.conf
 fi
