6a2383b32344b9b6cea155e9a45ddee2198ca2a8
Eric Fu <eric.fu@sdce.com.au>
2019-05-31 17:52:26 +1000

    add bitcoin-sv AKA bitcoin cash

---
+++ bitcoin-sv/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sed -i "s|rpcport=.*|rpcport=${RPC_PORT}|g" ./template.conf
+sed -i "s|rpcuser=.*|rpcuser=${RPC_USER}|g" ./template.conf
+sed -i "s|rpcpassword=.*|rpcpassword=${RPC_PASSWORD}|g" ./template.conf
+sed -i "s|rpcallowip=.*|rpcallowip=${RPC_ALLOW_IP}|g" ./template.conf
+sed -i "s|SYMBOL|${SYMBOL}|g" ./template.conf
+
+
+if [ "${ENV}" == "mainnet" ];
+then
+    /root/binary/bin/bitcoind -rescan -datadir=/root/mainnet_data -conf=/root/template.conf
+else
+    /root/binary/bin/bitcoind -rescan -regtest -datadir=/root/testnet_data -conf=/root/template.conf
+fi
